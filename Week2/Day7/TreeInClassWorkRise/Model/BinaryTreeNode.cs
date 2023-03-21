0868e0414ae2504fdcc84ff7137cad681635d5e2
Simona Emanuilova Stoyanova <simonaem99@gmail.com>
2023-03-21 14:03:19 +0200

    Added Task1 for Day 7 Week 2 Trees

---
+++ Week2/Day7/TreeInClassWorkRise/Model/BinaryTreeNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeInClassWorkRise.Model
+{
+    public class BinaryTreeNode
+    {
+        public BinaryTreeNode(int value, BinaryTreeNode left, BinaryTreeNode right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+
+        public int Value { get; set; }
+
+        public BinaryTreeNode Left { get; set; }
+
+        public BinaryTreeNode Right { get; set; }
+
+        public bool IsLeaf()
+        {
+            return (Right == null && Left == null);
+        }
+
+    }
+}
