0aba8dcc5272c310bd9577243ed85c436b4402a0
Simona Emanuilova Stoyanova <simonaem99@gmail.com>
2023-04-23 14:24:42 +0300

    Added Dockerize of .NET app (BE) for DevOps Day 1

--- Week5/Day 30/ToDoTask/ToDoTaskSolution/Entities/TODOTASKSContext.cs
+++ Week5/Day 30/ToDoTask/ToDoTaskSolution/Entities/TODOTASKSContext.cs
@@ -23,7 +23,7 @@ public partial class TODOTASKSContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-3JLTREN\\SQLEXPRESS01;Initial Catalog=TODOTASKS;Integrated Security=SSPI; Trust Server Certificate=True;");
+        => optionsBuilder.UseSqlServer("Server=sql-server-db,1433;Database=TODOTASKS;User ID=sa;Password=YourStrong@Passw0rd; Trust Server certificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
